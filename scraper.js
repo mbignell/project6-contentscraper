ebdd84aa75509156ed96bdf47f98c23dbdede203
Maggie Bignell <mbignell@users.noreply.github.com>
2018-01-14 00:10:36 -0800

    Removed to-dos

--- scraper.js
+++ scraper.js
@@ -40,17 +40,24 @@ function getTime() {
   return time;
 }
 
+// Error testing
+// printError({message : 'newone'});
+
+// Error function to use whenever an error occurs.
+// Creates Time stamp, formats error
+// creates file if one does not exist
+// appends error
 function printError(error) {
-  console.log(error.message);
-  try {
-    let currentTime = new Date();
-    let errorAppend = `[ ${currentTime} ] ${error.message}`
+  let currentTime = new Date();
+  let errorAppend = `[ ${currentTime} ] ${error.message} and ${error.response}\n`;
+  if (!fs.existsSync('scraper-error.log')){
+    fs.writeFile('scraper-error.log', '', (error) => {
+      fs.appendFileSync('scraper-error.log', errorAppend);
+    });
+  }else {
     fs.appendFileSync('scraper-error.log', errorAppend);
-    console.log('The error was appended to file!');
-  } catch (err) {
-    console.log('something wasnt appended shoot');
-  }
-}
+  };
+};
 
 // Testing time
 let currentTime = getTime();
@@ -66,27 +73,41 @@ x('http://shirts4mike.com/shirts.php', '.products li', [{
   // 'Time': getTime()
   // 'Time': currentTime (variable above)
   }])(function(error, shirts) {
-    // Add objects to CSV file with json2csv
-    try {
-      var csv = json2csv({ data: shirts, fields: fields });
-    } catch (error) {
-      // Errors are thrown for bad options, or if the data is empty and no fields are provided.
-      // Be sure to provide fields if it is possible that your data array will be empty.
-      printError(error);
-    }
-    // Writes (or rewrites) the CSV file with most recent data within the data folder
-    fs.writeFile(`./data/${getDate()}.csv`, csv, function(error) {
-      if (error) throw error;
-      console.log('file saved');
-    });
-})
-
-// TO DO
-
-// When an error occurs, log it to a file named scraper-error.log .
-//It should append to the bottom of the file with a time stamp and error e.g.
-//[Tue Feb 16 2016 10:02:12 GMT-0800 (PST)] <error message>
-
-//If http://shirts4mike.com is down, an error message describing the issue should appear in the console.
-  // The error should be human-friendly, such as “There’s been a 404 error. Cannot connect to the to http://shirts4mike.com.”
-  // To test and make sure the error message displays as expected, you can disable the wifi on your computer or device.
+    // Adding date to each item and editing title to remove price
+    // this adds it after the fact, yeah? how to do during processing?
+    if(!error){
+      for(let i= 0; i <= (shirts.length-1); i++){
+        shirts[i].Time = getTime();
+      }
+      // Add objects to CSV file with json2csv
+      try {
+        var csv = json2csv({ data: shirts, fields: fields });
+      } catch (error) {
+        // Errors are thrown for bad options, or if the data is empty and no fields are provided.
+        // Be sure to provide fields if it is possible that your data array will be empty.
+        let newError = new Error();
+        printError(newError);
+      }
+      // Writes (or rewrites) the CSV file with most recent data within the data folder
+      fs.writeFile(`./data/${getDate()}.csv`, csv, function(error) {
+        if (error) throw error;
+        console.log('file saved');
+      });
+    } else {
+      //If http://shirts4mike.com is down,
+      // an error message describing the issue should appear in the console.
+      // Else, normal error log.
+      if (error.response === 404) {
+        console.error(`There's been a 404 error OH NO! Cannot connect to Shirts 4 Mike.`)
+        printError(error);
+      } else {
+        console.error(error);
+        console.log(error.response);
+        // this is logging with an undefined response???????
+        //
+        // FIX
+        //
+        printError(error);
+      };
+    };
+});
